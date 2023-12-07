7d765ac1de2369254d61812ee28beeff86567b34
TYRON TAY THIO SHAO ZHE 2202023D <2202023D@student.tp.edu.sg>
2023-12-07 15:05:50 +0800

    Add project files.

---
+++ FinancialFinesse/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using FinancialFinesse.Server.Models;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace FinancialFinesse.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ FinancialFinesse/Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FinancialFinesse.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
